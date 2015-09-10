ad9ed662fa7ce1b9777c82dc42b0aff359870269
Ryan Tanay <ryantanay@newevolution.org>
2015-09-10 09:10:13 -0500

    Added a build to use the requirejs optimizer

---
+++ build.js
@@ -0,0 +1,23 @@
+({
+  paths: {
+    angularFilter: "../lib/bower_components/angular-filter/dist/angular-filter.min",
+    angularfire: "../lib/bower_components/angularfire/dist/angularfire.min",
+    bootstrap: "../lib/bower_components/bootstrap/dist/js/bootstrap.min",
+    angularRoute: "../lib/bower_components/angular-route/angular-route",
+    babel: "../lib/bower_components/requirejs-babel/babel-5.8.22.min",
+    jquery: "../lib/bower_components/jquery/dist/jquery.min",
+    angular: "../lib/bower_components/angular/angular.min",
+    firebase: "../lib/bower_components/firebase/firebase",
+    es6: "../lib/bower_components/requirejs-babel/es6"
+  },
+  shim: {
+    "angularfire" : ["angular", "firebase"],
+    "firebase": {"exports" : "Firebase"},
+    "angular" : {"exports" : "angular"},
+    "angularFilter" : ["angular"],
+    "angularRoute": ["angular"],
+    "bootstrap": ["jquery"]
+  },
+  name: "main",
+  out: "built.js"
+})
